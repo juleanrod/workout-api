1c689ba03dcd4d6e09f750107517f905989bc8ba
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-12-03 17:28:16 -0800

    Add database utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// body-parser allows us to parse the JSON inside the request body
+// we're able to receive the JSON data inside our controller under `req.body`
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => console.log(`API is listening in port ${PORT}`));
